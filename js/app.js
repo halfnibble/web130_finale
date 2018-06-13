b7c2f9910e0c67c48d009d62425472abbeda21bd
Joshua Wedekind <joshua@nostalg.io>
2018-06-13 17:48:18 +0000

    Fix the navbar up a bit.

--- js/app.js
+++ js/app.js
@@ -1,4 +1,7 @@
+/* global $ JS_PAGE Cookies */
+
 import './graphql/articles';
 import './graphql/login';
+import '../node_modules/popper.js/dist/popper.min.js';
 
 // import DarkSkyApi from '../node_modules/dark-sky-api/src/dark-sky-api';
